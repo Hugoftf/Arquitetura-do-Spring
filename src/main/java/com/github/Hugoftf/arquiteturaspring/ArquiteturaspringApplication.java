11e1e1a3adb20681c81f1744a018a1ab9e516900
Hugo de França Torres Ferreira <f3rreirahugo@hotmail.com>
2025-02-18 11:21:47 -0300

    Configuração da classe aplicação

--- src/main/java/com/github/Hugoftf/arquiteturaspring/ArquiteturaspringApplication.java
+++ src/main/java/com/github/Hugoftf/arquiteturaspring/ArquiteturaspringApplication.java
@@ -1,14 +1,38 @@
 package com.github.Hugoftf.arquiteturaspring;
 
+import org.h2.security.auth.Configurable;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ArquiteturaspringApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(ArquiteturaspringApplication.class, args);
+		//SpringApplication.run(ArquiteturaspringApplication.class, args);
+
+		SpringApplicationBuilder builder =  new SpringApplicationBuilder(ArquiteturaspringApplication.class);
+
+		builder.bannerMode(Banner.Mode.OFF);
+
+		builder.profiles("producao");
+		builder.run(args);
+
+		//Contexto da Applicação
+
+		ConfigurableApplicationContext context = builder.context();
+		//var produtoRepository = context.getBean("produtoRepository");
+
+		ConfigurableEnvironment environment = context.getEnvironment();
+
+		String appName =  environment.getProperty("string.application.name");
+		System.out.println("Nome da applicação: " + appName);
+
+
 	}
 
 }
